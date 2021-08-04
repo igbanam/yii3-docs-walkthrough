94ceb8bae8130cd4c96bc2f3a1688e58e3bf6410
Owajigbanam Ogbuluijah ✊🏾 <xigbanam@gmail.com>
2021-08-04 22:15:35 +0000

    Say Hello

---
+++ src/Controller/EchoController.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Yiisoft\Yii\View\ViewRenderer;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class EchoController {
+    private ViewRenderer $viewRenderer;
+
+    public function __construct(ViewRenderer $viewRenderer)
+    {
+        $this->viewRenderer = $viewRenderer->withControllerName('echo');
+    }
+
+    public function say(ServerRequestInterface $request): ResponseInterface
+    {
+        $message = $request->getAttribute('message', 'Hello!');
+
+        return $this->viewRenderer->render('say', [
+            'message' => $message,
+        ]);
+    }
+}
